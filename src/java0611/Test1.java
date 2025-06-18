eff009b976028a69656bc19f7fa0134d1636f10b
ByeonKangMin <dmlrud1154@gmail.com>
2025-06-18 12:18:22 +0900

    IfElse

--- src/java0611/Test1.java
+++ src/java0611/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
+        System.out.println("master");
     }
 }
